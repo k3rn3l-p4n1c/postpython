279d01e558c67e8f52444a3692a0ea06d936fef8
Bardia <az.bardia13@gmail.com>
2017-03-03 13:40:24 +0330

    Ready to be published on PyPI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='postpython',
+    packages=['postpython'],
+    version='1.0',
+    description='A library to use postman collection in python.',
+    author='Bardia Heydari nejad',
+    author_email='bardia.heydarinejad@gmail.com',
+    url='https://github.com/k3rn3l-p4n1c/postpython',
+    download_url='https://codeload.github.com/k3rn3l-p4n1c/postpython/zip/master',  # I'll explain this in a second
+    keywords=['postman', 'rest', 'api'],  # arbitrary keywords
+    classifiers=[],
+)
