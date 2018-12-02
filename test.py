2963bd7cebd9d50ae79116e57546bd47d44e3725
bardia <bardia.heydari@cafebazaar.ir>
2018-12-02 14:29:11 +0330

    Clean test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from postpython.core import PostPython
 
-pp = PostPython('/path/to/collection/Bazinama.postman_collection.json')
+pp = PostPython('/path/to/collection/Your.postman_collection.json')
 pp.environments.update({'BASE_URL': 'http://127.0.0.1:5000', 'PASSWORD': 'test', 'EMAIL': 'info@example.com'})
 pp.environments.update(pp.User.login().json()['message'])
 
